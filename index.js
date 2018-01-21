468c999d362a82feba42f81cbc5e34eb998a5402
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2018-01-21 13:42:47 -1000

    model matrix hooks

--- index.js
+++ index.js
@@ -4,13 +4,19 @@ module.exports = Groups
 
 function Groups (opts) {
   if (!(this instanceof Groups)) return new Groups(opts)
-  var N = 4096
+  this.N = 4096
+  var msize = getSize(64)
   this.data = {
-    positions: new Float32Array(N*3),
-    cells: new Uint32Array(N*3),
-    ids: new Float32Array(N),
+    positions: new Float32Array(this.N*3),
+    cells: new Uint32Array(this.N*3),
+    ids: new Float32Array(this.N),
+    models: new Float32Array(msize.length),
+    modelSize: [msize.width,msize.height],
+    modelTexture: null,
     count: 0
   }
+  this._mfns = []
+  this._updateTexture = opts.texture()
   this._voffsets = { _last: 0 }
   this._eoffsets = { _last: 0 }
   this._ids = { _last: -1 }
@@ -25,15 +31,34 @@ Groups.prototype.getId = function (name) {
   return id === undefined ? -1 : id
 }
 
+Groups.prototype.update = function () {
+  for (var i = 0; i < this._mfns.length; i++) {
+    var m = this.data.models.subarray(i*16,i*16+16)
+    this._mfns[i](m)
+  }
+  this.data.modelTexture = this._updateTexture({
+    data: this.data.models,
+    width: this.data.modelSize[0],
+    height: this.data.modelSize[1],
+    format: 'rgba',
+    wrapS: 'clamp',
+    wrapT: 'clamp'
+  })
+}
+
 Groups.prototype.add = function (name, mesh) {
   var positions = f32(mesh.positions)
   var cells = u32(mesh.cells)
   var id = ++this._ids._last
   this._ids[name] = id
+  if (id*16 >= this.data.models.length) {
+    throw new Error('TODO: resize model array')
+  }
+  this._mfns[id] = mesh.model
 
   var free = this.data.positions.length - this._lengths.positions
   if (positions.length > free) {
-    throw new Error('TODO: resize')
+    throw new Error('TODO: resize vertex array')
     /*
     var n = Math.log2(positions.length + free)
       this._lengths.positions)
