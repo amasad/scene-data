d2b38e326774cf5d6baef0a1c33531c875a6d490
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2018-01-29 12:47:22 -1000

    texture and attribute opts

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 var getSize = require('glsl-matrix-texture')
+var cons = {
+  float: Float32Array,
+  vec2: Float32Array, vec3: Float32Array, vec4: Float32Array,
+  mat2: Float32Array, mat3: Float32Array, mat4: Float32Array,
+  int8: Uint8Array, int16: Uint16Array, int32: Uint32Array,
+  uint8: Uint8Array, uint16: Uint16Array, uint32: Uint32Array
+}
 
 module.exports = Geometry
 
@@ -12,31 +19,51 @@ function Geometry (opts) {
   self.data = {
     positions: {
       data: new Float32Array(0),
+      type: 'vec3',
       count: 0
     },
     ids: {
       data: new Float32Array(0),
+      type: 'float',
       count: 0
     },
     elements: {
       data: new Uint32Array(0),
+      type: 'uint32[3]',
       count: 0
-    },
-    models: {
+    }
+  }
+  self._textureKeys = []
+  Object.keys(opts.textures || {}).forEach(function (key) {
+    var type = opts.textures[key]
+    self._textureKeys.push(key)
+    self.data[key] = {
+      data: new Float32Array(0),
+      type: type,
       texture: self._createTexture(),
       size: [0,0],
-      data: new Float32Array(0),
       update: function () {
-        self.data.models.texture({
-          data: self.data.models.data,
+        self.data[key].texture({
+          data: self.data[key].data,
+          type: 'float',
           format: 'rgba',
-          width: self.data.models.size[0],
-          height: self.data.models.size[1],
+          width: self.data[key].size[0],
+          height: self.data[key].size[1],
         })
       },
       count: 0
     }
-  }
+  })
+  self._attributeKeys = []
+  Object.keys(opts.attributes || {}).forEach(function (key) {
+    var type = opts.attributes[key]
+    self._attributeKeys.push(key)
+    self.data[key] = {
+      type: type,
+      data: new(cons[type])(0),
+      count: 0
+    }
+  })
 }
 
 Geometry.prototype.add = function (name, mesh) {
@@ -46,7 +73,10 @@ Geometry.prototype.add = function (name, mesh) {
   var cellCount = getCount(mesh.cells,3)
   this.data.elements.count += cellCount*3
   var id = this._idCount++
-  this.data.models.count++
+  for (var i = 0; i < this._textureKeys.length; i++) {
+    var key = this._textureKeys[i]
+    this.data[key].count++
+  }
   this._data.push({
     name: name,
     mesh: mesh,
@@ -66,11 +96,15 @@ Geometry.prototype.pack = function () {
   this.data.positions.data = new Float32Array(this.data.positions.count)
   this.data.ids.data = new Float32Array(this.data.ids.count)
   this.data.elements.data = new Uint32Array(this.data.elements.count)
-  var msize = getSize(this.data.models.count)
-  this.data.models.size[0] = msize.width
-  this.data.models.size[1] = msize.height
-  this.data.models.data = new Float32Array(msize.length)
 
+  for (var i = 0; i < this._textureKeys.length; i++) {
+    var key = this._textureKeys[i]
+    var msize = getSize(this.data[key].count)
+    this.data[key].size[0] = msize.width
+    this.data[key].size[1] = msize.height
+    this.data[key].data = new(cons[this.data[key].type])(msize.length)
+    this.data[key].update()
+  }
   for (var i = 0; i < this._data.length; i++) {
     var d = this._data[i]
     var pr = d.positionRange
@@ -87,6 +121,22 @@ Geometry.prototype.pack = function () {
         this.data.positions.data[pr[0]+j*3+2] = p[2]
       }
     }
+    for (var j = 0; j < this._attributeKeys.length; j++) {
+      var key = this._attributeKeys[i]
+      this.data[key].data = new Float32Array(this.data.positions.count)
+      this.data[key].count = this.data.positions.count
+      if (isFlat(d.mesh[key])) {
+        this.data[key].data.set(d.mesh[key],pr[0])
+      } else {
+        var l = d.mesh[key].length
+        for (var j = 0; j < l; j++) {
+          var p = d.mesh[key][j]
+          this.data[key].data[pr[0]+j*3+0] = p[0]
+          this.data[key].data[pr[0]+j*3+1] = p[1]
+          this.data[key].data[pr[0]+j*3+2] = p[2]
+        }
+      }
+    }
     if (isFlat(d.mesh.cells)) {
       this.data.elements.data.set(d.mesh.cells,cr[0])
     } else {
@@ -99,6 +149,7 @@ Geometry.prototype.pack = function () {
       }
     }
   }
+  this._data = []
 }
 
 Geometry.prototype.getId = function (name) {
